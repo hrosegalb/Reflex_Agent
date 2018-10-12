691eabf1f9cec4dc0ded11cff089219eaf72eaab
Hannah Galbraith <hannahrosegalbraith@gmail.com>
2018-10-12 11:30:55 -0700

    Cleaned up files a little bit

--- grid_square.py
+++ grid_square.py
@@ -1,5 +1,3 @@
-import random
-
 class Grid_Square(object):
     def __init__(self):
         self.dirty = False
--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-import numpy as np
+import random
 from grid_square import Grid_Square
 
 def main():
     num_rows = 3
     num_cols = 3
 
-    grid = [[Grid_Square() for i in range(3)], [Grid_Square() for i in range(3)], [Grid_Square() for i in range(3)]]
+    grid = [[Grid_Square() for j in range(num_cols)] for i in range(num_rows)]
 
-    for i in range(num_rows):
-        for j in range(num_cols):
-            print("{0},{1}".format(i, j))
-            print(grid[i][j].print_dirty())
+    # Randomly assign a dirty spot in the grid
+    dirty_spot = [random.randint(0, 2), random.randint(0, 2)]
+    grid[dirty_spot[0]][dirty_spot[1]].change_dirt_status(True)
+    print("({0},{1})".format(dirty_spot[0], dirty_spot[1]))
+    print(grid[dirty_spot[0]][dirty_spot[1]].print_dirty())
+
 
 if __name__ == '__main__':
     main()
